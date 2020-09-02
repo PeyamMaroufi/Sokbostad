5cdd0b8de45289d3181fb6dc65a820234eb08da3
Payam Maroufi <maroufi@kth.se>
2020-09-02 21:52:26 +0200

    (doc): The procedure of getting newest changes.

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import smtplib
+
+# Get the source of the page
+
+# replace out all the parts we don't need
+
+# calculate md5
+
+# compare the hash with the stored hash, different? do something
+
+# store the new hash
