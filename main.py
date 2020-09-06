16bf13684c3b4c8c7dd57ed83ac86f42391a9d98
Payam Maroufi <maroufi@kth.se>
2020-09-06 14:14:14 +0200

    (feat): Reading information of a website using BeautifulSoup

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
+import hashlib
+from bs4 import BeautifulSoup, SoupStrainer
 import requests
-from bs4 import BeautifulSoup
-import time
-import smtplib
+
 
 # Get the source of the page
+urls = "https://wahlinfastigheter.se/lediga-objekt/lagenheter/"
+r1 = requests.get(urls)
+print(r1.status_code)
+
+
+# Wåhlins fastighet
+if r1.status_code == 200:
+    coverpage = r1.content
+    soup1 = BeautifulSoup(coverpage, 'html.parser')
+    coverpage_news = soup1.find_all(
+        ['h3', 'span'], class_=['block-title', 'data'])
+    print(len(coverpage_news))
+    # print(coverpage_news)
+    for item in coverpage_news:
+        if item.name == 'span':
+            print('Data: ' + item.get_text())
+    # print(coverpage_news[0].get_text())
+
 
 # replace out all the parts we don't need
 
-# calculate md5
+# calculate mds Hash
+
 
 # compare the hash with the stored hash, different? do something
 
