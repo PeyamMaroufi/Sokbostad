5f8cea12b15c3f414a6f12fece75fdf6c3d2c82e
Payam Maroufi <maroufi@kth.se>
2020-09-08 22:15:33 +0200

    (feat): Img source address and links are now added. Unwanted object are removed

--- main.py
+++ main.py
@@ -5,29 +5,40 @@ import requests
 
 # Get the source of the page
 urls = "https://wahlinfastigheter.se/lediga-objekt/lagenheter/"
+base_url = "https://wahlinfastigheter.se"
 r1 = requests.get(urls)
 print(r1.status_code)
-
+url_h3 = []
+url_lazy = []
+url_img = []
 
 # Wåhlins fastighet
 if r1.status_code == 200:
+
     coverpage = r1.content
+
     soup1 = BeautifulSoup(coverpage, 'html.parser')
+
     coverpage_news = soup1.find_all(
-        ['h3', 'span'], class_=['block-title', 'data'])
+        ['h3', 'img'], class_=['block-title', 'lazy'])
+
     print(len(coverpage_news))
-    # print(coverpage_news)
+
     for item in coverpage_news:
-        if item.name == 'span':
-            print('Data: ' + item.get_text())
-    # print(coverpage_news[0].get_text())
 
+        if item.attrs['class'][0] == 'block-title' and (item.text not in ['\nParkering\n', '\nLokaler\n', '\nFörråd\n']):
 
-# replace out all the parts we don't need
+            temp_url = item.find('a', href=True)
+            url_h3.append(temp_url.attrs['href'])
 
-# calculate mds Hash
+        if item.name == 'img' and (item.attrs['alt'] not in ['Parkering', 'Lokaler', 'Förråd']):
 
+    print("Slut")
 
-# compare the hash with the stored hash, different? do something
+    # replace out all the parts we don't need
 
-# store the new hash
+    # calculate mds Hash
+
+    # compare the hash with the stored hash, different? do something
+
+    # store the new hash
