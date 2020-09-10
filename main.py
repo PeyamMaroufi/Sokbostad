c701796e296cb128ac0e3134028ef7e897ce5783
Payam Maroufi <maroufi@kth.se>
2020-09-10 22:30:28 +0200

    (feat): All information such as img url, link url and title are in a dictionary. title:(link, img) format

--- main.py
+++ main.py
@@ -2,43 +2,56 @@ import hashlib
 from bs4 import BeautifulSoup, SoupStrainer
 import requests
 
+items_walin = {}
 
-# Get the source of the page
-urls = "https://wahlinfastigheter.se/lediga-objekt/lagenheter/"
-base_url = "https://wahlinfastigheter.se"
-r1 = requests.get(urls)
-print(r1.status_code)
-url_h3 = []
-url_lazy = []
-url_img = []
 
-# Wåhlins fastighet
-if r1.status_code == 200:
+def walin_items_li():
 
-    coverpage = r1.content
+    # Get the source of the page
+    urls = "https://wahlinfastigheter.se/lediga-objekt/lagenheter/"
+    base_url = "https://wahlinfastigheter.se"
+    r1 = requests.get(urls)
+    print(r1.status_code)
+    url_h3 = []
+    url_img = []
 
-    soup1 = BeautifulSoup(coverpage, 'html.parser')
+    # Wåhlins fastighet
+    if r1.status_code == 200:
 
-    coverpage_news = soup1.find_all(
-        ['h3', 'img'], class_=['block-title', 'lazy'])
+        coverpage = r1.content
 
-    print(len(coverpage_news))
+        soup1 = BeautifulSoup(coverpage, 'html.parser')
 
-    for item in coverpage_news:
+        coverpage_news = soup1.find_all(
+            ['h3', 'img'], class_=['block-title', 'lazy'])
 
-        if item.attrs['class'][0] == 'block-title' and (item.text not in ['\nParkering\n', '\nLokaler\n', '\nFörråd\n']):
+        print(len(coverpage_news))
 
-            temp_url = item.find('a', href=True)
-            url_h3.append(temp_url.attrs['href'])
+        for item in coverpage_news:
 
-        if item.name == 'img' and (item.attrs['alt'] not in ['Parkering', 'Lokaler', 'Förråd']):
+            if item.attrs['class'][0] == 'block-title' and (item.text not in ['\nParkering\n', '\nLokaler\n', '\nFörråd\n']):
 
-    print("Slut")
+                temp_url = item.find('a', href=True)
+                temp_url_desc = {temp_url.get_text(): (temp_url.attrs['href'])}
+                url_h3.append(temp_url_desc)
 
-    # replace out all the parts we don't need
+            if item.name == 'img' and (item.attrs['alt'] not in ['Parkering', 'Lokaler', 'Förråd']):
+                url_img.append(base_url + item.attrs['data-src'])
 
-    # calculate mds Hash
+            # replace out all the parts we don't need
+        for item in range(0, len(url_h3)):
+            temp_keys = list(url_h3[item].keys())[0]
+            temp_values = list(url_h3[item].values())[0]
+            items_walin[temp_keys + ' ' +
+                        str(item + 1)] = (temp_values, url_img[item])
 
-    # compare the hash with the stored hash, different? do something
+        for key, value in items_walin.items():
+            print(key)
+            print(value)
+            print('------')
+            # calculate mds Hash
 
-    # store the new hash
+            # compare the hash with the stored hash, different? do something
+
+            # store the new hash
+walin_items_li()
