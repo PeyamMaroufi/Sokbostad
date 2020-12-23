567a97fa61a7f9537dd719f412331de4f43f6c74
Peyam Maroufi <maroufi@kth.se>
2020-12-23 20:18:14 +0100

    refactor: A smarter way to gather the apartments in a list

--- Bostadsok/Bostadsok/findApartment.py
+++ Bostadsok/Bostadsok/findApartment.py
@@ -6,46 +6,24 @@ from Heba import Heba
 import argparse
 
 """
-## DESCRIPTION
+# DESCRIPTION
 This file is only use to call each module and print out
 the results."""
 
 
-def walin_items():
+def walin_items(host_variable):
     x = 0
-    items = Walin.walin_items_li()
-    while items[x] is not None:
-        yield items[x].items()
-        x += 1
+    if host_variable == 0:
+        items = Hasselby.hasselby_items_li()
+    if host_variable == 1:
+        items = WasbyHem.wasby_items_li()
+    if host_variable == 2:
+        items = Akelius.akelius_items_li()
+    if host_variable == 3:
+        items = Heba.heba_items_li()
+    if host_variable == 4:
+        items = Walin.walin_items_li()
 
-
-def hasselby_items():
-    x = 0
-    items = Hasselby.hasselby_items_li()
-    while items[x] is not None:
-        yield items[x].items()
-        x += 1
-
-
-def wasbyhem_items():
-    x = 0
-    items = WasbyHem.wasby_items_li()
-    while items[x] is not None:
-        yield items[x].items()
-        x += 1
-
-
-def akelius_items():
-    x = 0
-    items = Akelius.akelius_items_li()
-    while items[x] is not None:
-        yield items[x].items()
-        x += 1
-
-
-def heba_items():
-    x = 0
-    items = Heba.heba_items_li()
     while items[x] is not None:
         yield items[x].items()
         x += 1
@@ -63,9 +41,13 @@ def main():
         "-a", "--all", help="List of all available housings from all hosts", required=False, dest="all_apartments", default=False)
     parser.add_argument(
         "-l", help="Show apartments from a specified host\n\tHosts:\n\tHeba\n\tHasselbyhem\n\tWaling\n\tWasbyhem\n\tAkelius", required=False, dest="host_var", type=str)
+    parser.add_argument(
+        "-y", help="Continuously output apartments from all hosts\n\tHosts:\n\tHeba\n\tHasselbyhem\n\tWaling\n\tWasbyhem\n\tAkelius", required=False, dest="output_yes", type=str)
+
     arguments = parser.parse_args()
     all_apartments = arguments.all_apartments
     host_var = arguments.host_var
+    output_yes = arguments.output_yes
 
     if all_apartments:
         print("hi")
