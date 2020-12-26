0dbbeff378c756230b2886a3064684c22eb209a7
Peyam Maroufi <maroufi@kth.se>
2020-12-26 23:30:47 +0100

    feat: Adding feature of const printing out the apartments

--- Bostadsok/Bostadsok/GenerateKeys.py
+++ Bostadsok/Bostadsok/GenerateKeys.py
@@ -1,10 +1,16 @@
 import hashlib
-import requests
+from urllib.request import urlopen
+import os
+import time
 
 
 class KeyGeneration:
     def __init__(self):
-        pass
+        self.user_path = os.path.expanduser('~/')
+        self.keys_path = os.path.join(self.user_path, ".bostadssokare/")
+        isDir = os.path.isdir(self.keys_path)
+        if not isDir:
+            os.mkdir(self.keys_path)
 
     def read_key(self):
         """
@@ -16,7 +22,7 @@ class KeyGeneration:
             hash_key        - string with key
         """
 
-        with open('hash_key.txt', 'r') as f:
+        with open(f'{self.keys_path}hash_key.txt', 'r') as f:
             hash_key = f.readline()
         return hash_key
 
@@ -29,19 +35,23 @@ class KeyGeneration:
         output:
             hash_key.txt    - txt file
         """
-        with open('hash_key.txt', 'w') as f:
+        with open(f'{self.keys_path}hash_key.txt', 'w') as f:
             f.write(key)
+        print("im here")
 
     def check_md5(self, url):
         """
         This function will check if the existing
         hash key is the same as the same.
         input:
-            url             - string website adress
+            url             - string website address
         output:
             none
         """
-        pages = requests.get(url)
-        hash_md5 = hashlib.sha256(pages.text.encode('utf-8')).hexdigest()
-        if not (hash_md5 == keys.read_key()):
-            self.create_md5(hash_md5)
+        if os.path.isfile(f'{self.keys_path}hash_key.txt'):
+            existing_keys = self.read_key()
+
+            pages = urlopen(url).read()
+            hash_md5 = hashlib.sha224(pages).hexdigest()
+            if existing_keys != hash_md5:
+                self.create_md5(hash_md5)
--- Bostadsok/Bostadsok/findApartment.py
+++ Bostadsok/Bostadsok/findApartment.py
@@ -1,9 +1,13 @@
+
 from Walin import Walin
 from Hasselby import Hasselby
 from WasbyHem import WasbyHem
 from Akelius import Akelius
 from Heba import Heba
 import argparse
+import concurrent.futures
+import time
+import os
 
 """
 # DESCRIPTION
@@ -11,32 +15,30 @@ This file is only use to call each module and print out
 the results."""
 
 
-def walin_items(host_variable):
-    x = 0
-    if host_variable == 0:
-        items = Hasselby.hasselby_items_li()
-    if host_variable == 1:
-        items = WasbyHem.wasby_items_li()
-    if host_variable == 2:
-        items = Akelius.akelius_items_li()
-    if host_variable == 3:
-        items = Heba.heba_items_li()
-    if host_variable == 4:
-        items = Walin.walin_items_li()
+def choose_host(host_variable):
+    if host_variable >= 0 and host_variable < 5:
+        if host_variable == 0 or host_variable == -1:
+            items1 = list(Walin.walin_items_li().items())
+            printAllApartment(items1)
+        elif host_variable == 1 or host_variable == -1:
+            items2 = list(Akelius.akelius_items_li().items())
+            printAllApartment(items2)
+        elif host_variable == 2 or host_variable == -1:
+            items3 = list(WasbyHem.wasby_items_li().items())
+            printAllApartment(items3)
+        elif host_variable == 3 or host_variable == -1:
+            items4 = list(Heba.heba_items_li().items())
+            printAllApartment(items4)
+        elif host_variable == 4 or host_variable == -1:
+            items5 = list(Hasselby.hasselby_items_li().items())
+            printAllApartment(items5)
+    else:
+        print(
+            "The host you provided does not exist. Enter en integer between [0-4] from:\n0. Wåhlin\n1. Akelius\n2. Wäsby\n3. Heba\n4. Hässelby")
 
-    while items[x] is not None:
-        yield items[x].items()
-        x += 1
 
-
-def printAllApartment():
-
-    items = [list(Walin.walin_items_li().items()),
-             list(Walin.walin_items_li().items()),
-             list(Hasselby.hasselby_items_li().items()),
-             list(WasbyHem.wasby_items_li().items()),
-             list(Akelius.akelius_items_li().items()),
-             list(Heba.heba_items_li().items())]
+def printTable():
+    # HElp variables
     Description = "Description"
     Host = "Host"
     Rooms = "Rooms"
@@ -44,73 +46,114 @@ def printAllApartment():
     print("--------------------------------------------------------------------------------------")
     print(f"{Description:{25}}{Host:{25}}{Rooms:{10}}{Size:{10}}Rent")
     print("--------------------------------------------------------------------------------------")
-    for item in items:
-        for i in item:
-            description = i[0][0:14]
-            start_string_page = i[1][0][i[1][0].find('/')+2:]
-            end_string_page = start_string_page[:start_string_page.find('/')]
-            if 'wahlinfastigheter' in end_string_page:
-                rooms = i[1][1].lstrip()
-                rooms = rooms.replace("rok", "") if "rok" in rooms else rooms
-                rooms = rooms.replace(" ", "")
-                size = i[1][2].lstrip()
-                size = size.replace("kvm", "") if "kvm" in size else size
-                rent = i[1][4].lstrip()
-                rent = rent.replace("kr/mån", "") if "kr/mån" in rent else rent
-                rent = rent.replace(
-                    "ad", "") if "ad" in rent else rent
 
-            elif 'akelius' in end_string_page:
-                description = description.lstrip()
-                rooms = i[1][2][0].lstrip()
-                rooms = rooms.replace("rum", "") if "rum" in rooms else rooms
-                rooms = rooms.replace(" ", "") if " " in rooms else rooms
-                size = i[1][2][1].lstrip()
-                size = size.replace("m²", "") if "m²" in size else size
-                rent = i[1][3].lstrip()
-                rent = rent.replace("SEK", "") if "SEK" in rent else rent
-                rent = rent.replace(" ", "") if " " in rent else rent
-            else:
-                rooms = i[1][2].lstrip()
-                size = i[1][3].lstrip()
-                rent = i[1][4].lstrip()
-                rent = rent.replace("Â", "") if "Â" in rent else rent
-            print(
-                f"{description:20}{end_string_page:30}{rooms:10}{size:10}{rent}")
+
+def printAllApartment(items):
+    # print each item from all hosts
+    for d in items:
+        description = d[0][0:14]
+        start_string_page = d[1][0][d[1][0].find('/')+2:]
+        end_string_page = start_string_page[:start_string_page.find('/')]
+        if 'wahlinfastigheter' in end_string_page:
+            rooms, size, rent = reformatLists(0, d)
+
+        elif 'akelius' in end_string_page:
+            description = description.lstrip()
+            rooms, size, rent = reformatLists(1, d)
+
+        else:
+            rooms, size, rent = reformatLists(2, d)
+        printApartmentsHelpFunction(
+            description, end_string_page, rooms, size, rent)
+
+
+def reformatLists(host, i):
+    # Wåhlin fastigheter
+    if host == 0:
+        rooms = i[1][1].lstrip()
+        rooms = rooms.replace("rok", "") if "rok" in rooms else rooms
+        rooms = rooms.replace(" ", "")
+        size = i[1][2].lstrip()
+        size = size.replace("kvm", "") if "kvm" in size else size
+        rent = i[1][4].lstrip()
+        rent = rent.replace("kr/mån", "") if "kr/mån" in rent else rent
+        rent = rent.replace("ad", "") if "ad" in rent else rent
+    # Akelius fastigheter
+    elif host == 1:
+        rooms = i[1][2][0].lstrip()
+        rooms = rooms.replace("rum", "") if "rum" in rooms else rooms
+        rooms = rooms.replace(" ", "") if " " in rooms else rooms
+        size = i[1][2][1].lstrip()
+        size = size.replace("m²", "") if "m²" in size else size
+        rent = i[1][3].lstrip()
+        rent = rent.replace("SEK", "") if "SEK" in rent else rent
+        rent = rent.replace(" ", "") if " " in rent else rent
+    # Other hosts
+    else:
+        rooms = i[1][2].lstrip()
+        size = i[1][3].lstrip()
+        rent = i[1][4].lstrip()
+        rent = rent.replace("Â", "") if "Â" in rent else rent
+
+    return rooms, size, rent
+
+
+def printApartmentsHelpFunction(description, end_string_page, rooms, size, rent):
+    print(f"{description:20}{end_string_page:30}{rooms:10}{size:10}{rent}")
 
 
 def main():
     parser = argparse.ArgumentParser(
         description='Print information on available housings.')
     parser.add_argument(
-        "-a", help="List of all available housings from all hosts", required=False, dest="all_apartments", default=False)
+        "-a", help="List of all available housings from all hosts", required=False, dest="all_apartments",  default=None, action='store_true')
     parser.add_argument(
-        "-l", help="Show apartments from a specified host\n\tHosts:\n\tHeba\n\tHasselbyhem\n\tWaling\n\tWasbyhem\n\tAkelius", required=False, dest="host_var", type=str)
+        "-l", help="Show apartments from a specified host:\n0. Wåhlin\n1. Akelius\n2. Wäsby\n3. Heba\n4. Hässelby", required=False, dest="host_integer", type=int)
     parser.add_argument(
-        "-y", help="Continuously output apartments from all hosts\n\tHosts:\n\tHeba\n\tHasselbyhem\n\tWaling\n\tWasbyhem\n\tAkelius", required=False, dest="output_yes", type=str)
+        "-y", help="Continuously output apartments from all hosts:\n0. Wåhlin\n1. Akelius\n2. Wäsby\n3. Heba\n4. Hässelby", required=False, dest="yes_integer", type=int, default=5)
 
     arguments = parser.parse_args()
     all_apartments = arguments.all_apartments
-    host_var = arguments.host_var
-    output_yes = arguments.output_yes
+    host_var = arguments.host_integer
+    yes = arguments.yes_integer
 
     if all_apartments:
-        printAllApartment()
+        hosts = [0, 1, 2, 3, 4]
+        printTable()
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            threads = [executor.submit(choose_host, host) for host in hosts]
+            for future in concurrent.futures.as_completed(threads):
+                try:
+                    _ = future.result()
+                except Exception as exc:
+                    print('Generated a stupid exception: %s' % (exc))
 
     if host_var is not None:
-        host_var = host_var.lower()
-        if host_var == 'akelius':
-            pass
-        if host_var == 'hasselby':
-            pass
-        if host_var == 'wasbyhem':
-            pass
-        if host_var == 'waling':
-            pass
-        if host_var == 'heba':
-            pass
+        choose_host(host_var)
+
+    if yes is not None:
+        hosts = [0, 1, 2, 3, 4]
+        try:
+            while True:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                printTable()
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                    threads = [executor.submit(choose_host, host)
+                               for host in hosts]
+                    for future in concurrent.futures.as_completed(threads):
+                        try:
+                            _ = future.result()
+                        except Exception as exc:
+                            print('Generated a stupid exception: %s' % (exc))
+                    del concurrent.futures.thread._threads_queues[list(executor._threads)[
+                        0]]
+                    time.sleep(yes)
+        except KeyboardInterrupt:
+            exit()
 
 
 if __name__ == '__main__':
-    # main()
-    printAllApartment()
+    main()
+    # printAllApartment()
+    # choose_host(-1)
